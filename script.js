0749e3b98529669fa6809e18a102c7663f239538
ghstdv-code <esguerragernel@gmail.com>
2022-04-10 16:29:47 +0800

    update layout

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const inputs = document.querySelectorAll('.form-group input[type="text"]'),
 btnCompute = document.querySelector('#btn-compute'),
 errorMsg = document.querySelector('.error'),
 result = document.querySelector('#average'),
-btnClear = document.querySelector('#btn-clear');
+btnClear = document.querySelector('#btn-clear'),
+wrapper = document.querySelector('.wrapper'),
+tableWrapper = document.querySelector('.wrapper.table-result'),
+table = document.querySelector('table');
 
 /*function IsDigit(event){
     if ((event.charCode >= 48 && event.charCode <= 57) || event.keyCode === 46)
@@ -10,8 +13,12 @@ btnClear = document.querySelector('#btn-clear');
     return false;
 }*/
 
+let datas = JSON.parse(localStorage.getItem("datas"));
+
 inputs.forEach((textField) => {
-    textField.maxLength = "5";
+    if(textField.name !== 'tbx-name'){
+        textField.maxLength = "5";
+    }
     textField.addEventListener('keyup', () => {
         errorMsg.style.display = "none";
         result.style.display = 'none';
@@ -37,8 +44,26 @@ btnCompute.addEventListener('click', () => {
             errorMsg.innerHTML = "Value must not be higher than 100 and lower than or equal to 0!";
             errorMsg.style.display = "block";
          } else {
-             result.innerHTML = `Final Grade: ${getAverage(score)}`;
-             result.style.display = 'block';
+            if(!datas){
+                datas = [];
+            }
+            let info = {name : inputs[0].value, 
+                prelim : inputs[1].value, 
+                midterm : inputs[2].value, 
+                prefinals: inputs[3].value, 
+                finals: inputs[4].value, 
+                finalGrade: getAverage(score)};
+            datas.push(info);
+            result.innerHTML = `Final Grade: ${getAverage(score)}`;
+            result.style.display = 'block';
+            wrapper.classList.add('active');
+            tableWrapper.style.display = "block"; 
+            localStorage.setItem('datas', JSON.stringify(datas));
+            showRows();
+
+            inputs.forEach((txt) => {
+                txt.value = "";
+            });
          }
      }
 
@@ -47,15 +72,42 @@ btnCompute.addEventListener('click', () => {
 btnClear.addEventListener('click', () => {
     inputs.forEach((txt) => {
         txt.value = "";
-        result.style.display = 'none';
-        errorMsg.style.display = "none";
     });
+    result.style.display = 'none';
+    errorMsg.style.display = "none";
+    tableWrapper.style.display = "none";
+    datas.splice(0, datas.length);
+    wrapper.style.height = "auto";
 });
 
 function getAverage(scores){
     let result = 0;
-    scores.forEach((score) => {
-        result += score;
+    for(let i = 1; i < scores.length; i++){
+        result += scores[i];
+    }
+    return (Math.round((result / (scores.length-1))*100)/100).toFixed(2);
+}
+
+function showRows(){
+    let tr = "";
+    datas.forEach((data) => {
+        tr += `<tr>
+                <td class='studName'>${data.name}</td>
+                <td class='term'>${data.prelim}</td>
+                <td class='term'>${data.midterm}</td>
+                <td class='term'>${data.prefinals}</td>
+                <td class='term'>${data.finals}</td>
+                <td>${data.finalGrade}</td>
+              </tr>`;
     });
-    return (Math.round((result / scores.length)*100)/100).toFixed(2);
+
+    table.innerHTML = ` <tr>
+                            <th>Student Name</th>
+                            <th class='term'>Prelim</th>
+                            <th class='term'>Midterm</th>
+                            <th class='term'>Pre-Finals</th>
+                            <th class='term'>Finals</th>
+                            <th>Final Grade</th>
+                        </tr>` + tr;
+    //tableWrapper.offsetHeight >= 170 ? tableWrapper.classList.add("overflow") : tableWrapper.classList.remove("overflow");
 }
